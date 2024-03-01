60abcda2f06471a8c79b4c8f137337cf8a13ac4f
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-03-01 17:24:02 +0100

    shifumi patch

--- shifumi/script.js
+++ shifumi/script.js
@@ -1,25 +1,43 @@
-// Tableau des choix de l'ordinateur
-const table = ['pierre', 'feuille', 'ciseaux'];
+// Déclaration constantes
+const play = document.querySelector('.btn-play');
+const btns = document.querySelectorAll('.btn');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+const result = document.querySelector('.result');
 
-// Choix du joueur et de l'ordinateur
-const joueur = document.getElementById('joueur');
-const ordi = document.getElementById('ordi');
+// Tableau items
+const items = ['pierre', 'feuille', 'ciseaux'];
 
-const pierre = document.getElementById('pierre');
-const feuille = document.getElementById('feuille');
-const ciseaux = document.getElementById('ciseaux');
+// Récupérer le symbole du joueur
+btns.forEach((btn) => btn.addEventListener('click', (e) => {
+    const playerItem = e.target.getAttribute('id');
+    player.textContent = playerItem;
+}))
 
-// Input du joueur
-const buttons = document.querySelectorAll('button');
+// Function startGame() 
+function startGame() {
+    // Générer un item aléatoire
+    const randomNum = Math.floor(Math.random() * items.length);
+    const randomItem = items[randomNum];
 
+    // Récupérer l'item du joueur
+    computer.textContent = randomItem;
+    const playerItem = player.textContent;
 
-buttons.forEach((button)=>{
-    button.addEventListener('click', () => {
-        console.log(console.dataset.type);
-        joueur.textContent = button.dataset.type;
-    });
-});
+    // Comparer les items entre ordi et joueur
+    if ((randomItem == 'pierre' && playerItem == 'feuille') ||
+        (randomItem == 'feuille' && playerItem == 'ciseaux') ||
+        (randomItem == 'ciseaux' && playerItem == 'pierre')) {
+            result.style.color = 'green';
+            result.textContent = 'Vicoire !';
+        } else if (randomItem == playerItem) {
+            result.style.color = 'gray';
+            result.textContent = 'Égalité';
+        } else {
+            result.style.color = 'red';
+            result.textContent = 'Perdu ...';
+    }
+}
 
-start.addEventListener('click', () => {
-    Math.floor(Math.random() * table.length);
-});
+// Écouteur d'événements sur le bouton Play 
+play.addEventListener('click', startGame);
